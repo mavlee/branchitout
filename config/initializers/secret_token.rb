dc48f3c4c1898b9e5c57f7d0b8a2c85dac7199c4
Chris Wu <chris.wu225@gmail.com>
2012-01-11 14:02:08 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Branchitout::Application.config.secret_token = '480b5c501b50f0f2d647e8dac53919d653f9cb0b5f3535e8b8ed1f90f8e43c9b7f5d04b4ccba7bfdf4fd6adfcb0f3a1f81052a5bae6809b58f4eb7650cbd50a2'
